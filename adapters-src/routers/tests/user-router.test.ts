0a3f24cf779a85411dd663a399d44f85762df81d
God-Im-Bored <aram_martin@alumni.brown.edu>
2023-03-26 16:46:29 -0500

    UDPATE: test spec for userrouter

--- adapters-src/routers/tests/user-router.test.ts
+++ adapters-src/routers/tests/user-router.test.ts
@@ -1,62 +1,111 @@
-import { Sign } from 'crypto'
-import request from 'supertest'
-import { User } from '../../../business-src/entities'
-import { Signup } from '../../../business-src/interfaces/useCases/user/user-signup'
-import { Login } from '../../../business-src/interfaces/useCases/user/user-login'
-import { Logout } from '../../../business-src/interfaces/useCases/user/user-logout'
-import { DeleteAccount } from '../../../business-src/interfaces/useCases/user/user-delete-account'
-import UserRouter from '../user-router'
-import server from './test-server'
+import { Sign } from "crypto";
+import request from "supertest";
+import { User } from "../../../business-src/entities";
+import { Signup } from "../../../business-src/interfaces/useCases/user/user-signup";
+import { Login } from "../../../business-src/interfaces/useCases/user/user-login";
+import { Logout } from "../../../business-src/interfaces/useCases/user/user-logout";
+import { DeleteAccount } from "../../../business-src/interfaces/useCases/user/user-delete-account";
+import UserRouter from "../user-router";
+import server from "./test-server";
 
 class MockUserSignUpUseCase implements Signup {
-    verifyAuth(email: String, password: String): Promise<boolean> {
-        throw new Error("Authorization method not implemented")
-    }
-    execute(email: String, password: String): Promise<boolean> {
-        throw new Error("Execute method not implemented")
-    }
+  verifyAuth(email: String, password: String): Promise<boolean> {
+    throw new Error("Authorization method not implemented");
+  }
+  execute(email: String, password: String): Promise<boolean> {
+    throw new Error("Execute method not implemented");
+  }
 }
 
 class MockUserLoginUseCase implements Login {
-    execute(email: String, password: String): Promise<User> {
-        throw new Error("Execute method not implemented")
-    }
+  execute(email: String, password: String): Promise<User> {
+    throw new Error("Execute method not implemented");
+  }
 }
 
 class MockUserLogoutUseCase implements Logout {
-    execute(email: String, password: String): Promise<boolean> {
-        throw new Error("Execute method not implemented")
-    }
+  execute(email: String, password: String): Promise<boolean> {
+    throw new Error("Execute method not implemented");
+  }
 }
 
 class MockUserDeleteAccountUseCase implements DeleteAccount {
-    execute(): Promise<boolean> {
-        throw new Error("Execute method not implemented")
-    }
+  execute(): Promise<boolean> {
+    throw new Error("Execute method not implemented");
+  }
 }
 
-
 describe("User Router", () => {
-    let mockUserSignUpUseCase: MockUserSignUpUseCase
-    let mockUserLoginUseCase: MockUserLoginUseCase
-    let mockUserLogoutUseCase: MockUserLogoutUseCase
-    let mockUserDeleteAccountUseCase: MockUserDeleteAccountUseCase
+  let mockUserSignUpUseCase: MockUserSignUpUseCase;
+  let mockUserLoginUseCase: MockUserLoginUseCase;
+  let mockUserLogoutUseCase: MockUserLogoutUseCase;
+  let mockUserDeleteAccountUseCase: MockUserDeleteAccountUseCase;
 
-    beforeAll(() => {
-        mockUserSignUpUseCase = new MockUserSignUpUseCase()
-        mockUserLoginUseCase = new MockUserLoginUseCase()
-        mockUserLogoutUseCase = new MockUserLogoutUseCase()
-        mockUserDeleteAccountUseCase = new MockUserDeleteAccountUseCase()
+  beforeAll(() => {
+    mockUserSignUpUseCase = new MockUserSignUpUseCase();
+    mockUserLoginUseCase = new MockUserLoginUseCase();
+    mockUserLogoutUseCase = new MockUserLogoutUseCase();
+    mockUserDeleteAccountUseCase = new MockUserDeleteAccountUseCase();
 
-        server.use('/user', UserRouter(mockUserSignUpUseCase, mockUserLogoutUseCase,  mockUserLoginUseCase, mockUserDeleteAccountUseCase))
-    })
+    server.use(
+      "/api",
+      UserRouter(
+        mockUserSignUpUseCase,
+        mockUserLogoutUseCase,
+        mockUserLoginUseCase,
+        mockUserDeleteAccountUseCase
+      )
+    );
+  });
 
-    beforeEach(() => {
-        jest.clearAllMocks()
-    })
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("User signs up [creates an account]", () => {
+    test("POST /api/signup verifyAuth()", async () => {
+      const userData = {
+        id: "1",
+        email: "john_doe@gmail.com",
+        password: "123456",
+        firstName: "John",
+        lastName: "Doe",
+        getFullName: "John Doe",
+        birthday: "940101",
+        isAdmin: false,
+        createdAt: null,
+      };
 
 
+      jest
+        .spyOn(mockUserSignUpUseCase, "verifyAuth")
+        .mockImplementation(() => {
+            throw new Error()
+        });
 
+      const response = await request(server).post("/api/signup").send(userData);
+      expect(response.status).toBe(200);
+    });
 
+    test("POST /api/signup execute()", async () => {
+      const userData = {
+        id: "1",
+        email: "john_doe@gmail.com",
+        password: "123456",
+        firstName: "John",
+        lastName: "Doe",
+        getFullName: "John Doe",
+        birthday: "940101",
+        isAdmin: false,
+        createdAt: null,
+      };
 
-})
+      jest
+        .spyOn(mockUserSignUpUseCase, "execute")
+        .mockImplementation(() => Promise.reject('error'));
+
+      const response = await request(server).post("/api/signup").send(userData);
+      expect(response.status).toBe(201);
+    });
+  });
+});
--- adapters-src/routers/user-router.ts
+++ adapters-src/routers/user-router.ts
@@ -1,59 +1,66 @@
-import express, { Request, Response} from "express"
-import { DeleteAccount, Login, Logout, Signup } from "../../business-src/interfaces/useCases/user"
+import express, { Request, Response } from "express";
+import {
+  DeleteAccount,
+  Login,
+  Logout,
+  Signup,
+} from "../../business-src/interfaces/useCases/user";
 
 export default function UserRouter(
-    signupUseCase: Signup,
-    logoutUseCase: Logout,
-    loginUseCase: Login,
-    deleteAccountUseCase: DeleteAccount
+  signupUseCase: Signup,
+  logoutUseCase: Logout,
+  loginUseCase: Login,
+  deleteAccountUseCase: DeleteAccount
 ) {
-    const router = express.Router()
+  const router = express.Router();
 
-    // implement a method(s) that'll create, check and delete token from sessions
+  // implement a method(s) that'll create, check and delete token from sessions
 
-    router.post('/signup', async (req: Request, res: Response) => {
-        try {
-            await signupUseCase.verifyAuth(req.body.email, req.body.password)
-            res.statusCode = 200
-            res.json({ message: "Account authentication was successful." })
-        } catch (err) {
-            res.status(500).send({ message: "Error verifying authentication. "})
-        }
+  router.post("/signup", async (req: Request, res: Response) => {
+    if (!(await signupUseCase.verifyAuth(req.body.email, req.body.password))) {
+      res.status(400).send({
+        message: "Data entry not valid. Try again.",
+      });
+    } else {
+      return res.status(200);
+    }
 
-        try {
-            await signupUseCase.execute(req.body.email, req.body.password)
-            res.statusCode = 201
-            res.json({ message: "New user account created." })
-        } catch (err) {
-            res.status(500).send({ message: "Error saving data. "})
-        }
-    })
+    try {
+      console.log(req.body.email, req.body.password);
+      await signupUseCase.execute(req.body.email, req.body.password);
+      res.statusCode = 201;
+      res.json({ message: "New user account created." });
+    } catch (err) {
+      return res.status(500).json({ message: "Error saving data." });
+    }
+  });
 
-    router.get('/login', async (req: Request, res: Response) => {
-        try {
-            const user = await loginUseCase.execute(req.body.email, req.body.password)
+  router.get("/login", async (req: Request, res: Response) => {
+    try {
+      const user = await loginUseCase.execute(
+        req.body.email,
+        req.body.password
+      );
 
-            // req.session.id = user.id
-            res.status(200).send(user)
-        } catch (err) {
-            res.status(500).send({ message: "Error fetching data" })
-        }
-    })
+      // req.session.id = user.id
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(500).send({ message: "Error fetching data" });
+    }
+  });
 
-    
-    router.post('/logout', async (req: Request, res: Response) => {
-        try {
-            await logoutUseCase.execute(req.body.email, req.body.password)
-            // req.logout() 
-            // req.session.destroy()
-            // res.sendStatus(204)
-            
-        } catch (err) {
-            res.status(500).send({ message: "Error saving data." })
-        }
-    })
+  router.post("/logout", async (req: Request, res: Response) => {
+    try {
+      await logoutUseCase.execute(req.body.email, req.body.password);
+      // req.logout()
+      // req.session.destroy()
+      // res.sendStatus(204)
+    } catch (err) {
+      res.status(500).send({ message: "Error saving data." });
+    }
+  });
 
-    return router
+  return router;
 }
 
-module.exports = UserRouter
+module.exports = UserRouter;
